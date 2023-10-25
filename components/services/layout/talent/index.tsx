3e699655bc92f5595428f7c7c1484d9e4bd71c49
valentina buoro <valentinabuoro4@gmail.com>
2023-10-25 14:51:53 +0100

    feat: services card components

--- components/services/layout/talent/index.tsx
+++ components/services/layout/talent/index.tsx
@@ -1,6 +1,14 @@
 import Link from "next/link";
 import Image from "next/image";
 import Button from "@/components/micro/button";
+import Card from "@/components/micro/services/card";
+import {CgNotes} from 'react-icons/cg'
+import IndustryExpertise from "../../../../public/assets/services/industryExpertise.svg"
+import Talents from "../../../../public/assets/services/techTalent.svg"
+import Feedback from "../../../../public/assets/services/feedback.svg"
+import StreamlinedProcess from '../../../../public/assets/services/streamlinedProcess.svg'
+import ContinousFeedback from '../../../../public/assets/services/continousFeedback.svg'
+import FlexibleHiringModels from '../../../../public/assets/services/flexibleHiringModels.svg'
 
 const Talent = () => {
   return (
@@ -32,6 +40,30 @@ const Talent = () => {
             <Button className="my-5">Get In Touch</Button>
           </Link>
         </div>
+      </div>
+      <div>
+      <div className="lg:pt-10 pt-5 grid grid-cols-1 lg:grid-cols-12 gap-4">
+        <div className="lg:col-span-4">
+          <Card src={Talents} text="Access to a vast and diverse network of tech professionals at all experience levels." title="Tech Talent" />
+        </div>
+        <div className="lg:col-span-4">
+          <Card src={IndustryExpertise} text="In-depth knowledge of the tech industry, enabling precise candidate matching." title="Industry Expertise" />
+        </div>
+        <div className="lg:col-span-4">
+          <Card src={Feedback} text="Tailored recruitment solutions to match the specific needs and culture of tech organizations." title="Customized Hiring" />
+        </div>
+      </div>
+      <div className="lg:pt-10 pt-5 grid grid-cols-1 lg:grid-cols-12 gap-4">
+        <div className="lg:col-span-4">
+          <Card src={FlexibleHiringModels} text=" Options for full-time, part-time, contract, or remote hiring to meet various needs." title="Flexible Hiring Models" />
+        </div>
+        <div className="lg:col-span-4">
+          <Card src={ContinousFeedback} text="A feedback loop for continuous improvement and candidate-client alignment." title="Continous Feedback" />
+        </div>
+        <div className="lg:col-span-4">
+          <Card src={StreamlinedProcess} text="Efficient and time-saving recruitment processes to minimize hiring timelines." title="Streamlined Process" />
+        </div>
+      </div>
       </div>
       <hr className="lg:mt-20 mt-14 opacity-10" />
     </section>
